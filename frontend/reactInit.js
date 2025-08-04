d91741e23f7b1d2a5288fa583b575376a0ff4930
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-04 17:58:04 +0800

    moved setups based on their folders

---
+++ frontend/reactInit.js
@@ -0,0 +1,59 @@
+export default async function initReact() {
+  const initReact = await select({
+    message:
+      "Would you like to initialize a React project with Vite?\n If no, this will initialize a React project with Create React App.",
+    default: "yes",
+    choices: [
+      { value: "yes", name: "Yes" },
+      { value: "no", name: "No" },
+    ],
+  });
+  const reactAppName = await input({
+    message: "What would you like to name your React app?",
+    default: "my-app",
+  });
+  const reactTypescript = await select({
+    message: "Would you like to use TypeScript with your React app?",
+    default: "no",
+    choices: [
+      { value: "yes", name: "Yes" },
+      { value: "no", name: "No" },
+    ],
+  });
+
+  if (initReact === "yes") {
+    console.log(
+      chalk.green("Initializing React project with Vite (React Router)..."),
+    );
+    exec(
+      `npx npx create-react-router@latest ${reactAppName} ${reactTypescript === "yes" ? "" : "--template remix-run/react-router-templates/javascript"}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(chalk.red(`Error: ${error.message}`));
+          return;
+        }
+        if (stderr) {
+          console.error(chalk.yellow(`Warning: ${stderr}`));
+        }
+        console.log(chalk.green(`Output: ${stdout}`));
+      },
+    );
+  } else {
+    console.log(
+      chalk.green("Initializing React project with Create React App..."),
+    );
+    exec(
+      `npx create-react-app ${reactAppName} ${reactTypescript === "yes" ? "--template typescript" : ""}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(chalk.red(`Error: ${error.message}`));
+          return;
+        }
+        if (stderr) {
+          console.error(chalk.yellow(`Warning: ${stderr}`));
+        }
+        console.log(chalk.green(`Output: ${stdout}`));
+      },
+    );
+  }
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import chalk from "chalk";
 import { select, input } from "@inquirer/prompts";
 import { exec } from "child_process";
+import reactInit from "./frontend/react.js";
 console.clear();
 console.log(
   "\
@@ -41,63 +42,7 @@ switch (answer) {
     });
     console.log(chalk.green(`You selected ${answerFrontend}!`));
     if (answerFrontend === "react") {
-      const initReact = await select({
-        message:
-          "Would you like to initialize a React project with Vite?\n If no, this will initialize a React project with Create React App.",
-        default: "yes",
-        choices: [
-          { value: "yes", name: "Yes" },
-          { value: "no", name: "No" },
-        ],
-      });
-      const reactAppName = await input({
-        message: "What would you like to name your React app?",
-        default: "my-app",
-      });
-      const reactTypescript = await select({
-        message: "Would you like to use TypeScript with your React app?",
-        default: "no",
-        choices: [
-          { value: "yes", name: "Yes" },
-          { value: "no", name: "No" },
-        ],
-      });
-
-      if (initReact === "yes") {
-        console.log(
-          chalk.green("Initializing React project with Vite (React Router)..."),
-        );
-        exec(
-          `npx npx create-react-router@latest ${reactAppName} ${reactTypescript === "yes" ? "" : "--template remix-run/react-router-templates/javascript"}`,
-          (error, stdout, stderr) => {
-            if (error) {
-              console.error(chalk.red(`Error: ${error.message}`));
-              return;
-            }
-            if (stderr) {
-              console.error(chalk.yellow(`Warning: ${stderr}`));
-            }
-            console.log(chalk.green(`Output: ${stdout}`));
-          },
-        );
-      } else {
-        console.log(
-          chalk.green("Initializing React project with Create React App..."),
-        );
-        exec(
-          `npx create-react-app ${reactAppName} ${reactTypescript === "yes" ? "--template typescript" : ""}`,
-          (error, stdout, stderr) => {
-            if (error) {
-              console.error(chalk.red(`Error: ${error.message}`));
-              return;
-            }
-            if (stderr) {
-              console.error(chalk.yellow(`Warning: ${stderr}`));
-            }
-            console.log(chalk.green(`Output: ${stdout}`));
-          },
-        );
-      }
+      await reactInit();
     }
     // Here you would call the function to create the frontend stack
 
