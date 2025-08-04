3f38fcd71096eb2c1c223a55319a3c504d98b1e8
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-05 02:18:17 +0800

    updated svelte init

--- frontend/svelteInit.js
+++ frontend/svelteInit.js
@@ -1,9 +1,13 @@
 import runCommand from "../utils/runCommand.js";
-
+import { input } from "@inquirer/prompts";
 export default async function svelteInit() {
+  const svelteAppName = await input({
+    message: "What would you like to name your Svelte app?",
+    default: "my-svelte-app",
+  });
   await runCommand(
     "npx",
-    ["sv", "create"],
+    ["sv", "create", svelteAppName, "--template", "library"],
     "Successfully initialized Svelte project.",
   );
 }
