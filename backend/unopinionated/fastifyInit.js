693c09cf32d4402facc201c9410320cc3056592e
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-04 23:41:19 +0800

    fixed typo

--- backend/unopinionated/fastifyInit.js
+++ backend/unopinionated/fastifyInit.js
@@ -2,7 +2,7 @@ import runCommand from "../../utils/runCommand.js";
 import { input } from "@inquirer/prompts";
 export default async function fastifyInit() {
   const projectName = await input({
-    message: "Enter the name of your Express.js project:",
+    message: "Enter the name of your Fastify project:",
     default: "my-app",
   });
   await runCommand("npm", ["init", "fastify", projectName]);
