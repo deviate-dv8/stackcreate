6c3cdebec67010a2abda03ca9689aeca03c811c5
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-04 21:17:44 +0800

    removed unused imports

--- frontend/angularInit.js
+++ frontend/angularInit.js
@@ -1,6 +1,4 @@
-import { spawn } from "child_process";
 import chalk from "chalk";
-import { input, select } from "@inquirer/prompts";
 import runCommand from "../utils/runCommand.js";
 
 export default async function angularInit() {
--- frontend/solidInit.js
+++ frontend/solidInit.js
@@ -1,6 +1,3 @@
-import { spawn } from "child_process";
-import chalk from "chalk";
-import { input, select } from "@inquirer/prompts";
 import runCommand from "../utils/runCommand.js";
 
 export default async function solidInit() {
