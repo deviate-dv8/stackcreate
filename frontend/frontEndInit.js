d2a2be3ceb8fa526bac16f7c96b4c2013434b2b8
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-04 18:27:39 +0800

    added vue initialize

--- frontend/frontEndInit.js
+++ frontend/frontEndInit.js
@@ -1,6 +1,7 @@
 import { select } from "@inquirer/prompts";
 import chalk from "chalk";
 import reactInit from "./reactInit.js";
+import vueInit from "./vueInit.js";
 export default async function frontEndInit() {
   console.log(chalk.blue("You selected Frontend!"));
   const answerFrontend = await select({
@@ -15,8 +16,29 @@ export default async function frontEndInit() {
       { value: "vite", name: "Initalize with Vite" },
     ],
   });
-  console.log(chalk.green(`You selected ${answerFrontend}!`));
-  if (answerFrontend === "react") {
-    await reactInit();
+  switch (answerFrontend) {
+    case "react":
+      console.log(chalk.blue("You selected React!"));
+      await reactInit();
+      break;
+    case "vue":
+      console.log(chalk.green("You selected Vue!"));
+      await vueInit();
+      break;
+    case "angular":
+      console.log(chalk.red("You selected Angular!"));
+      break;
+    case "svelte":
+      console.log(chalk.hex("#FFA500")("You selected Svelte!"));
+      break;
+    case "solid":
+      console.log(chalk.blue("You selected Solid!"));
+      break;
+    case "vite":
+      console.log(chalk.blue("You selected Vite!"));
+      break;
+    default:
+      console.error(chalk.red("Invalid option selected."));
+      process.exit(1);
   }
 }
