57fe1a853e504aba4e12c70f560b0344c7bd83b4
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-08-05 03:11:42 +0800

    updated command for solid

--- frontend/solidInit.js
+++ frontend/solidInit.js
@@ -1,9 +1,28 @@
 import runCommand from "../utils/runCommand.js";
-
+import { input, select } from "@inquirer/prompts";
 export default async function solidInit() {
+  const solidAppName = await input({
+    message: "What would you like to name your Solid app?",
+    default: "my-solid-app",
+  });
+  const typeScriptSupport = await select({
+    message: "Would you like to use TypeScript?",
+    default: "yes",
+    choices: [
+      { value: "yes", name: "Yes" },
+      { value: "no", name: "No" },
+    ],
+  });
   await runCommand(
     "npm",
-    ["init", "solid"],
+    [
+      "create",
+      "vite@latest",
+      solidAppName,
+      "--",
+      "--template",
+      `${typeScriptSupport === "yes" ? "solid-ts" : "solid"}`,
+    ],
     "Successfully initialized Solid project.",
   );
 }
